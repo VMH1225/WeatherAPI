62a48de5f649c09eed95f5ac6b12e54ff1ae2004
VMH1225 <huertavictor1225@gmail.com>
2022-10-08 11:39:14 -0700

    added event listeners for each country on click

--- js/main.js
+++ js/main.js
@@ -7,14 +7,68 @@ const options = {
 	}
 };
 
+let choice = ''
+document.querySelector('#italy').addEventListener('click', milanForecast)
+document.querySelector('#japan').addEventListener('click', tokyoForecast)
+document.querySelector('#usa').addEventListener('click', newYorkForecast)
+document.querySelector('#gb').addEventListener('click', londonForecast)
+document.querySelector('#france').addEventListener('click', parisForecast)
 
-fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=newyork&format=json&u=f', options)
-.then(res => res.json())
-.then(data => {
-	console.log(data)
-})
-.catch(err =>{
-	console.log(`Error ${err}`)
-})
+function milanForecast(){
+	console.log(`She's from italia`)
+	fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=milan&format=json&u=f', options)
+	.then(res => res.json())
+	.then(data => {
+		console.log(data)
+		document.querySelector('#currentTemp').innerHTML = data.current_observation.condition.temp
+	})
+	.catch(err =>{
+		console.log(`Error ${err}`)
+	})
+	
+}
 
+function tokyoForecast(){
+		fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=tokyo&format=json&u=f', options)
+			.then(res => res.json())
+			.then(data => {
+				console.log(data)
+			})
+			.catch(err =>{
+				console.log(`Error ${err}`)
+			})
+}
+
+function newYorkForecast(){
+		fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=newyork&format=json&u=f', options)
+				.then(res => res.json())
+				.then(data => {
+					console.log(data)
+				})
+				.catch(err =>{
+					console.log(`Error ${err}`)
+				})
+}
+
+function londonForecast(){
+		fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=london&format=json&u=f', options)
+				.then(res => res.json())
+				.then(data => {
+					console.log(data)
+				})
+				.catch(err =>{
+					console.log(`Error ${err}`)
+				})
+}
+
+function parisForecast(){
+		fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=paris&format=json&u=f', options)
+				.then(res => res.json())
+				.then(data => {
+					console.log(data)
+				})
+				.catch(err =>{
+					console.log(`Error ${err}`)
+				})
+}
 
