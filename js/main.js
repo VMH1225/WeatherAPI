17476df422a7286f309f149f4b0a6ef28c951f8d
VMH1225 <huertavictor1225@gmail.com>
2022-10-10 13:25:45 -0700

    added responsivenss to flag display

--- js/main.js
+++ js/main.js
@@ -20,14 +20,14 @@ function milanForecast(){
 	.then(res => res.json())
 	.then(data => {
 		console.log(data)
-		document.querySelector('#current > span').innerHTML = data.current_observation.condition.temperature
-		document.querySelector('#current span ~ span').innerHTML = data.current_observation.condition.text
+		document.querySelector('#current > span').innerHTML = `Current Weather: ${data.current_observation.condition.temperature}`
+		document.querySelector('#current span ~ span').innerHTML =  data.current_observation.condition.text
 		weekWeather(data.forecasts)
 	})
 	.catch(err =>{
 		console.log(`Error ${err}`)
 	})
-	
+
 }
 
 function tokyoForecast(){
@@ -89,11 +89,12 @@ function parisForecast(){
 
 function weekWeather(week){
 	console.log(week)
+	document.querySelector('#forecast').style.display = 'flex'
 	let day = 1
 	for(let i=0; i < 7; i++){
 		document.querySelector(`#day${day} h3`).innerHTML = week[i].day
-		document.querySelector(`#day${day} span`).innerHTML =  week[i].high
-		document.querySelector(`#day${day} span ~ span`).innerHTML = week[i].low
+		document.querySelector(`#day${day} span`).innerHTML =  `High: ${week[i].high}`
+		document.querySelector(`#day${day} span ~ span`).innerHTML = `Low: ${week[i].low}`
 		document.querySelector(`#day${day} .text`).innerHTML =  week[i].text
 		day++
 	}
